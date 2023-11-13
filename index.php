6fcf251611db9b868a38b48ae46ec7e1c22d862e
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-11-13 23:13:19 +0100

    default font weight applied

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ Plugin Name: FCP Gutenberg Blocks
 Plugin URI: https://github.com/VVolkov833/gutenberg-blocks
 GitHub Plugin URI: https://github.com/VVolkov833/gutenberg-blocks
 Description: Add various gutenberg blocks
-Version: 0.0.7
+Version: 0.0.9
 Requires at least: 6.0
 Tested up to: 6.3
 Requires PHP: 7.4
