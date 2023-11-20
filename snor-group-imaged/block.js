a2dd548828fb222ef03672408c7cae4ac3f17521
VVolkov833 <vadim.volkov.dev@gmail.com>
2023-11-20 10:02:21 +0100

    block with background image

---
+++ snor-group-imaged/block.js
@@ -0,0 +1,167 @@
+(() => {
+
+	const el = wp.element.createElement;
+
+
+	wp.blocks.registerBlockType( blockName, {
+		title,
+        icon: iconSrc
+			? el('img', {
+				src: iconSrc,
+				alt: title,
+				width: 20,
+				height: 20,
+			})
+			: 'columns',
+		category: 'widgets',
+
+		attributes: {
+			images: {
+                type: 'array',
+                default: []
+            },
+			align: {
+				type: 'string',
+				default: 'full',
+			},
+			left: {
+				type: 'number',
+				default: 0
+			},
+			top: {
+				type: 'number',
+				default: 0
+			},
+			size: {
+				type: 'number',
+				default: 50
+			}
+		},
+
+        supports: {
+			align: ['wide', 'full'],
+			color: {
+				gradients: true,
+			},
+			spacing: {
+				margin: ['top', 'bottom'],
+				padding: true
+			},
+        },
+
+		edit: props => {
+
+			const onSelectImage = (media, ind) => {
+                const images = [...props.attributes.images];
+                const newImage = {
+                    id: media.id,
+                    url: media.url,
+					thumbnail: media.sizes.large ? media.sizes.large.url : media.url, // .thumbnail
+                };
+                images[ind] = newImage;
+                props.setAttributes({ images });
+			};
+
+            const mediaBox = ind => {
+                const images = [...props.attributes.images];
+                const mediaID = images[ind]?.id || 0;
+				const mediaThumbnail = images[ind]?.thumbnail || images[ind]?.url;
+                return [
+                    'figure',
+                    {},
+                    el( wp.blockEditor.MediaUpload, {
+                        onSelect: media => { onSelectImage(media, ind) },
+                        allowedTypes: 'image',
+                        value: mediaID,
+                        render: obj => {
+                            return el(
+                                wp.components.Button,
+                                {
+                                    type: 'button',
+                                    className: 'select-image ' + (mediaID || 'empty-image'),
+                                    onClick: obj.open,
+                                },
+                                mediaID
+                                    ? el( 'img', { src: mediaThumbnail } )
+                                    : el( 'span', {}, 'Image' ),
+                                mediaID
+                                    ? el( 'button', { type: 'button', className: 'clear-image', onClick: e => {
+                                        e.stopPropagation();
+                                        images[ind].id = 0;
+                                        images[ind].url = '';
+                                        props.setAttributes({ images });
+                                      }}, '+' )
+                                    : null,
+                            );
+                        },
+                    })
+                ];
+
+            };
+
+            let style = {};
+            if ( props.attributes.left ) { style['--left'] = `${props.attributes.left}%`; }
+			if ( props.attributes.top ) { style['--top'] = `${props.attributes.top}%`; }
+			if ( props.attributes.size ) { style['--size'] = `${props.attributes.size}%`; }
+			if ( props.attributes.images[0]?.url ) { style['--backgroundImage'] = `url('${props.attributes.images[0]?.url}')`; }
+			return el( 'div',
+				{ className: `${props.className} ${prefix}main`, style },
+				el( wp.blockEditor.InnerBlocks, {}),
+				el( wp.element.Fragment, // sidebar
+					{},
+					el( wp.blockEditor.InspectorControls, {},
+						el( wp.components.PanelBody, { className: `${prefix}main`, style },
+							el( wp.components.RangeControl, {
+								label: 'X position',
+								value: props.attributes.left || 0,
+								onChange: value => {
+									props.setAttributes( { left: value } );
+								},
+								min: -50,
+								max: 150
+							}),
+							el( wp.components.RangeControl, {
+								label: 'Y position',
+								value: props.attributes.top || 0,
+								onChange: value => {
+									props.setAttributes( { top: value } );
+								},
+								min: -50,
+								max: 150
+							}),
+							el( wp.components.RangeControl, {
+								label: 'Size %',
+								value: props.attributes.size || 0,
+								onChange: value => {
+									props.setAttributes( { size: value } );
+								},
+								min: 0,
+								max: 100
+							}),
+							el( ...mediaBox(0) ),
+						)
+					),
+				)
+			);
+		},
+		save: props => {
+			const image = props.attributes.images[0]?.url
+				? el( 'img', { src: props.attributes.images[0].url, alt: 'Background image', title: 'Background image' } )
+				: null;
+			const span = image && !props.attributes.link
+				? el( 'span', { className: `${prefix}image` }, image )
+				: null;
+
+			let style = {};
+            if ( props.attributes.left ) { style['--left'] = `${props.attributes.left}%`; }
+			if ( props.attributes.top ) { style['--top'] = `${props.attributes.top}%`; }
+			if ( props.attributes.size ) { style['--size'] = `${props.attributes.size}%`; }
+			if ( props.attributes.minHeight ) { style['--minHeight'] = `${props.attributes.minHeight}px`; }
+			return el( 'div',
+				{ className: `${prefix}main`, style },
+                el( wp.blockEditor.InnerBlocks.Content ),
+				span,
+            );
+		},
+	} );
+})();
