c8ca13232dfdbc02718f2d4cba3637f5119e3631
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-04-29 20:56:36 +0200

    forgot to add class names

--- blocks/lanuwa-contact-section/index.php
+++ blocks/lanuwa-contact-section/index.php
@@ -1,5 +1,5 @@
 <?php $print_function = function($props) { ?>
-    <div class="alignfull">
+    <div class="alignfull <?php echo $props['className'] ?>">
     <?php include(locate_template('template-parts/contact-section.php')); ?>
     </div>
 <?php };
