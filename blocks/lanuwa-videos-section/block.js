93945b333e6a8e2101e4485845be0ee31ff8ef70
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-08-29 21:47:51 +0200

    add video-section block

---
+++ blocks/lanuwa-videos-section/block.js
@@ -0,0 +1,40 @@
+(() => {
+
+	const el = wp.element.createElement;
+	const attributes = { // to have the access to the default values
+		align: {
+			type: 'string',
+			default: 'full',
+		},
+	};
+
+	wp.blocks.registerBlockType( blockName, {
+		title,
+        icon: iconSrc
+			? el('img', {
+				src: iconSrc,
+				alt: title,
+				width: 20,
+				height: 20,
+			})
+			: 'columns',
+		category: 'widgets',
+
+		attributes,
+
+        supports: {
+			align: ['full'],
+        },
+
+		edit: props => {
+
+			return el( 'div',
+				{ className: `${props.className} ${prefix}main`, style: { width: '100%', padding: '30px 0', backgroundColor: 'var(--wp--preset--color--grey-lighter)', textAlign: 'center' } },
+				'A list of latest videos is printed instead of me',
+			);
+		},
+		save: props => {
+			return null;
+		},
+	} );
+})();
---
+++ blocks/lanuwa-videos-section/index.php
@@ -0,0 +1,5 @@
+<?php $print_function = function($props) { ?>
+    <div class="alignfull <?php echo $props['className'] ?>">
+    <?php include(locate_template('template-parts/home-main-video-section.php')) ?>
+    </div>
+<?php };
