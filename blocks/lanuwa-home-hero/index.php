87c5e449de0c4b2e24141175ee70914bc4c86b32
VVolkov833 <vadim.volkov.dev@gmail.com>
2024-09-05 20:12:09 +0200

    change bg image delivery

--- blocks/lanuwa-home-hero/index.php
+++ blocks/lanuwa-home-hero/index.php
@@ -1,11 +1,6 @@
 <?php $print_function = function($props, $content) { ?>
-<style>
-    .home-hero-image {
-        --backgroundImage: url('<?php echo $props['images'][0]['url'] ?? '' ?>');
-    }
-</style>
 <section class="section-content home-hero-section alignfull">
-    <div class="home-hero-image" style="background: var(--backgroundImage) no-repeat center center;"></div>
+    <div class="home-hero-image" style="--backgroundImage:url('<?php echo $props['images'][0]['url'] ?? '' ?>')"></div>
     <div class="home-hero-container container">
         <div class="col-md-6 col-md-offset-6">
             <div class="content">
