7fc1823bd304d0d75089170b45c68adb42c11d9e
VVolkov833 <vadim.volkov.dev@gmail.com>
2025-06-16 22:27:08 +0200

    adjust tabs logic & styling

--- blocks/lanuwa-tabs-tab/index.php
+++ blocks/lanuwa-tabs-tab/index.php
@@ -1,9 +1,42 @@
-<?php $print_function = function($props, $content) { ?>
-<section class="">
-    <pre>
 <?php
-print_r([$props, $content]);
+$print_function = function(array $props, string $content): void {
+    global $fc_gutenberg_blocks_tabs_id, $fc_gutenberg_blocks_tabs_collected;
+
+    if ( empty($props['tabLabel']) && !$content ) {
+        return;
+    }
+    if ( empty($props['tabLabel']) ) {
+        $props['tabLabel'] = '-----';
+    }
+
+    $first = !$fc_gutenberg_blocks_tabs_id;
+    $fc_gutenberg_blocks_tabs_id = $fc_gutenberg_blocks_tabs_id ?: substr(uniqid(), -6);
+    $fc_gutenberg_blocks_tabs_collected = $fc_gutenberg_blocks_tabs_collected ?? [];
+
+    $tabs_id = 'tabs' . $fc_gutenberg_blocks_tabs_id;
+    $tab_id = 'tab' . $fc_gutenberg_blocks_tabs_id . count($fc_gutenberg_blocks_tabs_collected);
+
+    $fc_gutenberg_blocks_tabs_collected[$tab_id] = $props['tabLabel'];
 ?>
-    </pre>
-</section>
+
+    <input
+        type="radio"
+        name="<?php echo $tabs_id ?>"
+        class="sr-only"
+        id="<?php echo $tab_id ?>"
+        <?php echo $first ? 'checked' : '' ?>
+        role="tab"
+        aria-labelledby="label<?php echo $tab_id ?>"
+    >
+    <label
+        id="label<?php echo $tab_id ?>"
+        for="<?php echo $tab_id ?>"
+    >
+        <?php echo esc_html($props['tabLabel']) ?>
+    </label>
+    <div>
+        <?php echo $content ?>
+    </div>
+
 <?php };
+?>
--- blocks/lanuwa-tabs/index.php
+++ blocks/lanuwa-tabs/index.php
@@ -1,9 +1,19 @@
-<?php $print_function = function($props, $content) { ?>
-<section class="">
-    <pre>
 <?php
-print_r([$props, $content]);
+$print_function = function(array $props, string $content): void {
+    global $fc_gutenberg_blocks_tabs_id, $fc_gutenberg_blocks_tabs_collected;
 ?>
-    </pre>
+
+<section class="fc-tabs" role="tablist">
+    <?php echo $content ?>
+    <nav>
+        <?php foreach ($fc_gutenberg_blocks_tabs_collected as $tab_id => $label) { ?>
+            <label for="<?php echo $tab_id ?>"><?php echo esc_html($label) ?></label>
+        <?php } ?>
+    </nav>
 </section>
-<?php };
+
+<?php 
+    $fc_gutenberg_blocks_tabs_id = '';
+    $fc_gutenberg_blocks_tabs_collected = [];
+};
+?>
