fef6c49a0ce7c6f4baaf5320804161032aaf39e2
VVolkov833 <vadim.volkov.dev@gmail.com>
2025-06-17 14:23:25 +0200

    change the template, forgot

--- blocks/lanuwa-callback-section/index.php
+++ blocks/lanuwa-callback-section/index.php
@@ -1,5 +1,5 @@
 <?php $print_function = function($props) { ?>
     <div class="alignwide <?php echo $props['className'] ?>">
-    <?php include(locate_template('template-parts/contact-section.php')); ?>
+    <?php include(locate_template('template-parts/callback-section.php')); ?>
     </div>
 <?php };
